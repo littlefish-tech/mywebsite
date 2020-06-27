7d7098202487b910370cfbae0fa83b7a5e25340a
Yimeng Yu <yimeng.yu@gmail.com>
2020-06-27 15:54:27 -0700

    add php file

---
+++ process.php
@@ -0,0 +1,15 @@
+<?php
+  if (isset($_POST['email']))  {
+  
+    //Email information
+    $admin_email = "yimeng.yu@gmail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    //send email
+    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+
+    header('Location: https://littlefish-tech.github.io/mywebsite.github.io/');
+  }
